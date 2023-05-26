5dd0f633be04b46c9c1847fd30bc34e19f2bece7
MaheshHunt <mahi.hunt@yahoo.com>
2023-05-26 05:39:13 +0530

    Main.java

---
+++ ParkingLot-LLD/src/Main.java
@@ -0,0 +1,15 @@
+package com.sysd.parking;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ParkingLot parkingLot = new ParkingLot(2,2, 2);
+		
+		Vehicle truck = new Vehicle(VehicleType.TRUCK);
+		parkingLot.parkVehicle(truck);
+		parkingLot.unParkVehicle(truck);
+		
+
+	}
+
+}
